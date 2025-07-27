cc400049a04427f7a1fa151b0362ccf69119c5ee
Rezkaudi <audirezk@gmail.com>
2025-07-27 23:54:59 +0300

    add blog 11 and add new product

--- src/content/blogs/blogs.ts
+++ src/content/blogs/blogs.ts
@@ -9,11 +9,13 @@ import blog7 from "./blog7.json";
 import blog8 from "./blog8.json";
 import blog9 from "./blog9.json";
 import blog10 from "./blog10.json";
+import blog11 from "./blog11.json";
+
 
 
 
 // Create array of blog posts
-const blogs = [blog1, blog2, blog3, blog4, blog5, blog6, blog7, blog8, blog9, blog10];
+const blogs = [blog1, blog2, blog3, blog4, blog5, blog6, blog7, blog8, blog9, blog10, blog11];
 
 // Export the array as default
 export default blogs;
--- src/content/product_details/products.ts
+++ src/content/product_details/products.ts
@@ -5,7 +5,8 @@ import product4 from './自動車整備機械/塗装ブース.json'
 import product5 from './プリンター・ミシン・DIY用機器/UVインクジェットプリンタ.json'
 import product6 from './プリンター・ミシン・DIY用機器/レーザー加工機 . 彫刻機.json'
 import product7 from './工作機械・精密加工機械/汎用旋盤.json'
+import product8 from './工作機械・精密加工機械/ボール盤.json'
 
-const products = [product1, product2, product3, product4, product5, product6, product7]
+const products = [product1, product2, product3, product4, product5, product6, product7, product8]
 
 export default products
