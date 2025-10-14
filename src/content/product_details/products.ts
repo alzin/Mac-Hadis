f9e6f3653826737e5730c14be9fb8cd76248bb1f
Hakam aldeen Alkhadraa <hakamha8@gmail.com>
2025-10-14 14:35:44 +0300

    add new page

--- src/content/product_details/products.ts
+++ src/content/product_details/products.ts
@@ -8,7 +8,8 @@ import product7 from './工作機械・精密加工機械/汎用旋盤.json'
 import product8 from './工作機械・精密加工機械/ボール盤.json'
 import product9 from './金属加工機械・板金機械/ロール成形機 . ハゼ折り機.json'
 import product10 from './金属加工機械・板金機械/マルチワーカー.json'
+import product11 from './発電機・溶接機・コンプレッサ/エンジン発電機.json'
 
-const products = [product1, product2, product3, product4, product5, product6, product7, product8, product9, product10]
+const products = [product1, product2, product3, product4, product5, product6, product7, product8, product9, product10, product11]
 
 export default products
