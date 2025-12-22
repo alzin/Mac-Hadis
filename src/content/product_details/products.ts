8c5b4458cf312c3f31096e79a7a5570031071618
Hakam aldeen Alkhadraa <hakamha8@gmail.com>
2025-12-22 23:20:50 +0300

    build 荷揚機(瓦揚機) product page

--- src/content/product_details/products.ts
+++ src/content/product_details/products.ts
@@ -13,7 +13,8 @@ import product12 from './発電機・溶接機・コンプレッサ/スクリュ
 import product13 from './自動車整備機械/フレーム修正機.json'
 import product14 from './自動車整備機械/Snap-on スナップオン.json'
 import product15 from './住宅工事機械工具・木工機械/万能木工機.json'
+import product16 from './住宅工事機械工具・木工機械/荷揚機(瓦揚機).json'
 
-const products = [product1, product2, product3, product4, product5, product6, product7, product8, product9, product10, product11, product12, product13, product14, product15]
+const products = [product1, product2, product3, product4, product5, product6, product7, product8, product9, product10, product11, product12, product13, product14, product15, product16]
 
 export default products
